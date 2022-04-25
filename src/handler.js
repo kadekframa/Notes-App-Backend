bcc292eb79952b60bb244316a53a3d93d2b0ce1e
kadekframa <dframa19@gmail.com>
2022-04-25 09:30:32 +0800

    Notes App Backend. Menghapus Catatan using Hapi framework. Simple RESTful API using Hapi Framework sudah berhasil dibangun :)

--- src/handler.js
+++ src/handler.js
@@ -103,5 +103,35 @@ const editNoteByIdHandler = (request, h) => {
     
 }
 
+const deleteNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
 
-module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler };
+    const index = notes.findIndex((note) => note.id === id);
+
+    if(index !== -1){
+        notes.splice(index, 1);
+        const response = h.response({
+            status: 'success',
+            message: 'Catatan berhasil dihapus',
+        });
+
+        response.code(200);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Catatan gagal dihapus. Id tidak ditemukan',
+    });
+
+    response.code(404);
+    return response;
+};
+
+
+module.exports = {addNoteHandler,
+    getAllNotesHandler,
+    getNoteByIdHandler,
+    editNoteByIdHandler,
+    deleteNoteByIdHandler
+};
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,8 @@ const {
     addNoteHandler,
     getAllNotesHandler,
     getNoteByIdHandler,
-    editNoteByIdHandler
+    editNoteByIdHandler,
+    deleteNoteByIdHandler
 } = require('./handler');
 
 const routes = [
@@ -25,6 +26,11 @@ const routes = [
         method: 'PUT',
         path: '/notes/{id}',
         handler: editNoteByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
     },
 ];
 
