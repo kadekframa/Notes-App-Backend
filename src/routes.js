76b8d8dc3e59c510a7ea671c0c68ef81f40158f1
kadekframa <dframa19@gmail.com>
2022-04-25 08:54:01 +0800

    Notes App Backend. Menyimpan catatan dan menampilkan catatan using Hapi framework

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/notes/',
+        handler: () => {},
+    },
+];
+
+module.exports = routes;
